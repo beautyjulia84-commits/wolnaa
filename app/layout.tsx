4123d9bbb6ed4c9e3f4af4077b276bd16cb76e8c
Helmut Hieb <helmuthieb@iMac-von-Helmut.fritz.box>
2026-06-09 04:19:39 +0200

    fix: body bg-black entfernt

--- app/layout.tsx
+++ app/layout.tsx
@@ -111,7 +111,7 @@ export default function RootLayout({
           `}
         </Script>
       </head>
-      <body className="min-h-full flex flex-col bg-black text-white">
+      <body className="min-h-full flex flex-col text-white">
         {children}
       </body>
     </html>
